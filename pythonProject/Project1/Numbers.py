5b50c873f9481ff2211c8b2ca96a009d86d2d633
Romeo Asinari <ralonsoad@gmail.com>
2023-04-08 12:26:16 +0200

    Python_Numbers

---
+++ pythonProject/Project1/Numbers.py
@@ -0,0 +1,15 @@
+from math import *
+
+
+print(-2.5 + 2)
+my_num=-5
+print(abs(my_num))
+
+print(pow(4,6))
+
+print(max(4,6))
+print(min(4,6))
+print(round(5.7))
+
+print(floor(6.8))
+print(ceil(6.8))
