fb54c97884d515a0f06678b62f1debdc82f0bbf6
Romeo Asinari <ralonsoad@gmail.com>
2023-04-19 09:05:26 +0200

    Functions

---
+++ pythonProject/Project1/Calculator.py
@@ -0,0 +1,4 @@
+num1= input("write a number: ")
+num2= input("write a second number: ")
+result=float(num1)+ float(num2)
+print(result)
---
+++ pythonProject/Project1/Functions.py
@@ -0,0 +1,15 @@
+
+#create and call a function
+def say_hi(name,age):
+    print(" Hellow "+ name +", you are "+ age )
+
+say_hi("Romeo","35")
+say_hi("Mike","40")
+
+def cube(num):
+     return num*num*num
+
+result = cube(4)
+print(result)
+
+
